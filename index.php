6a67e46155f61e13c5387a00022925e87dd1debf
TheoEssoh <essohjean88@gmail.com>
2019-05-02 04:20:16 +0300

    page d'accueil

---
+++ index.php
@@ -0,0 +1,231 @@
+<!DOCTYPE html public " -//w3cdtd xhtml 1.0 transitional//EN" "http://www.w3.org/TR/xhtml1-transitional.dtd">
+      <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr"> 
+             <head> 
+<!-- En-tête du document  -->
+                  <title> Laboratoire d’Analyse  micrographique</title>
+                   <!-- Balise meta  -->
+                   <!-- <meta name="keywords" content="Choi quality control, mots-clé2, micrographique, Laboratoire d’Analyse  micrographique..."/>-->
+                  <meta name="description" content="Laboratoire d’Analyse micrographique situé au sein du campus de KER LANN met son savoir-faire à disposition des industriels et des particuliers pour tester la fiabilité de leurs produits en vous proposant une expertise d’analyse micrographique pour assurer la qualité de vos produits electroniques et mécaniques et la stabilité de votre ligne de production.">
+                  <meta name="keywords" content="Laboratoire d’Analyse micrographique,Tests électroniques et mécaniques, électronique industrielle, mécanique industrielle, Microscopie électronique à balayage,MEB, Analyse Micrographique, Analyse de produits électroniques, Analyse de produits mécaniques, Choi quality control, Controle qualité électronique, controle qualité mecanique, Enceinte, Climatique, Thermiques, Enceinte, Choc, Thermique, Test Mécanique, Test électronique, Mécanique, electronique, Norme, Iso 9001, Iso 17025, Essai, Essais, Climatiques, Thermique, peinture, Salin, Vieillisement, UV,vitre, Rayure, Abrasion, CEM, Compatibilite, Electomagnétique, Déverminage, CRT, CEI 68, IEC 68, PSA, Rennes, Bretagne,Ille,Vilaine,bruz, france" lang="fr">
+                  <meta name="google-site-verification" content="-CAiD8B9dXihe904XVV0lb4btA4CIHOOjfo0nSKeq2U">
+                  <meta name="msvalidate.01" content="60B680C9C6316BD9FFD848C13629DA7B">
+                  <!-- Indexer et suivre -->
+                  <meta name="robots" content="index,follow" />
+                  <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+                  <meta name="author" content="Choi quality control">
+                  <meta name="google-site-verification" content="-CAiD8B9dXihe904XVV0lb4btA4CIHOOjfo0nSKeq2U">
+                  <meta name="msvalidate.01" content="60B680C9C6316BD9FFD848C13629DA7B"> 
+                  <meta name="viewport" content="initial-scale=1.0, user-scalable=yes" />              
+                   <!-- Indexer et suivre -->
+                  <meta name="robots" content="index,follow" />
+
+                  <!-- F-icon -->
+                  <link rel="shortcut icon" href="images/favicon.ico">
+                  <link rel="icon" type="image/gif" href="images/animated_favicon1.gif">
+                  <!--  Relier une feuille CSS  -->
+
+                  <link rel="stylesheet" type="text/css" media="print, screen" href="css/style.css">
+                  <link rel="stylesheet" type="text/css" media="print, screen" href="css/classe.css">
+                  <link rel="stylesheet" type="text/css" media="print, screen" href="css/police.css">
+                  <link rel="stylesheet" type="text/css" media="print, screen" href="css/formulaire.css">
+                  <link rel="stylesheet" type="text/css" media="print, screen" href="css/tableau.css">
+                  <link rel="stylesheet" type="text/css" media="print" href="css/print.css">
+                  <link rel='stylesheet' type="text/css" media="print, screen" href='bootstrap/css/bootstrap.css' />
+                  <link rel="stylesheet" type="text/css" media="print, screen" href="css/testcss.css">
+                  <link rel="stylesheet" type="text/css" media="print, screen" href="css/accordeon.css">
+                  <!--<link rel="stylesheet" type="text/css" media="print, screen" href="css/page2.css"> -->                                
+                  <!-- Incoporez du JavaScript dans la page  -->
+                  <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+                  <script type="text/javascript" src="js/jquery.js"></script>
+                  <script type="text/javascript" src="http://code.jquery.com/jquery-1.8.2.min.js"></script>                
+             </head> 
+
+             <body>
+              <div class="container">
+                <div class="row">
+                  <nav class="navbar navbar-inverse navbar-fixed-top">
+                    <!--header-->
+                    <header>
+                      <div class="container">
+                        <div class="row">
+                          <div class="col-xs-1 col-md-3 col-sm-2 col-lg-2">
+                            <a href="index.php" title="retour a l'accueil">
+                              <img id="logo" src="images/logo.jpg" alt="Choi Quality Control" width="250" height="100" />
+                            </a>
+                          </div>
+                          <h1 id="titre" class="titre">LABORATOIRE D'ANALYSE MICROGRAPHIQUE</h1>
+                          <div class="col-xs-1 col-md-2 col-sm-3 col-lg-3">
+                            <div id="accueil-entete">
+                              <a href="accueilang.php" title="Lire la page en Anglais">
+                                <img id="ang" src="images/an.png" alt="Anglais" width="30" height="20">
+                              </a>
+                            </div>
+                          </div>
+                        </div>
+                      </header>
+                      <!--Accueil-->
+                      <div id ="accueil-principal" class="container">
+                        <!-- Menu-->
+                        <div id="menu-accueil" class="row">
+                          <div id="conteneur" class="col-xs-2 col-md-2 col-sm-3 col-lg-3">
+                            <div id="wrap">
+                              <div id="sidebar" class="">
+                                <!--Ceci est ma colonne latérale -->
+                                <!-- CORPS DE LA PAGE  -->
+                                <ul class="navigation">
+                                  <li id="index">
+                                    <a class="in" href="index.php" title="Retour à l'accueil" accesskey="1">ACCUEIL</a>
+                                  </li>
+                                  <li id="ste">
+                                    <a class="in" href="societefran.php" title="La société Choi quality control : historique et articles de presse" accesskey="1">LA SOCI&Eacute;T&Eacute;</a>
+                                  </li>
+                                  <li id="formation"><a href="qualitefran.php" title="prestations">LA QUALITE</a></li>
+                                  <li id="formation"><a href="prestationfran.php" title="prestations">PRESTATIONS</a></li>
+                                  <li id="formation"><a href="formationfran.php" title="Formations">AUTRES SERVICES</a></li>
+                                  <li id="acces"><a href="accesfran.php" title="plan d'accès">PLAN D'ACC&Egrave;S</a></li>
+                                  <li id="contact"><a href="contactfran1.php" title="Contact">CONTACT</a></li>
+                                  <!--les partenaires-->
+                                  <div id="bg">
+                                    <a href="partenaires.php" title="liste des partenanires et clients">
+                                      <h2>partenaires</h2>
+                                      <img id="tab1" src="images/image1.png" alt="Image1"/>
+                                      <img id="tab2" src="images/image2.png" alt="Image2"/>
+                                      <img id="tab3" src="images/image3.png" alt="Image3"/>
+                                      <img id="tab4" src="images/image4.png" alt="Image4"/>
+                                      <img id="tab5" src="images/image5.png" alt="Image5"/>
+                                    </a>
+                                  </div>
+                                </ul>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                      
+                      
+                          <!--fin menu-->
+                          <!--contenu-->
+                          <div class="col-xs-4 col-md-5 col-sm-6 col-lg-6">
+                            <div id="contenu-accueil">
+                              <ul>
+                                <li id="paragraphe">
+                                 <p><a href="societefran.php" title="informations sur la société">Choi Quality Control</a> (CQC) vous propose une expertise d’analyses micrographiques pour assurer la qualité de vos produits et aussi la stabilité de votre ligne de production.</br>
+                                  Les services d’analyses vous proposent:</br>
+                                  </p>
+                                </li>
+                              </ul>
+                              <!--<p><strong class="nom">  </strong>  -->
+                              <ul>
+                                <li id="contenu1"><a href="contenu1.html" title="Coupe micrographique">Coupe micrographique,</a>
+                                </li>
+                                <li id="contenu2"><a href="contenu2.html" title="Analyse micrographique">Analyse micrographique, MEB,</a>
+                                </li>
+                                <li id="contenu3"><a href="contenu3.html" title="DPA">DPA (destructive physical analysis),</a>
+                                </li>
+                                <li id="contenu4"><a href="contenu4.html" title="Désenrobage">Désenrobage, dédorage,</a>
+                                </li>
+                                <li id="contenu5"><a href="contenu5.html" title="Reverse engineering">Reverse engineering,</a>
+                                </li>
+                                <li id="contenu6"><a href="contenu6.html" title="Non destructive analyse">Non destructive analyse (Rx-2d, Rx-3d, ultrason),</a>
+                                </li>
+                                <li id="contenu7"><a href="contenu7.html" title="Analyse thermique">Analyse thermique (TD, ATD),</a>
+                                </li>
+                                <li id="contenu8"><a href="contenu8.html" title="Analyse chimique (IR)">Analyse chimique (IR),</a>
+                                </li>
+                                <li id="contenu9"><a href="contenu9.html" title="Analyse mécanique">Analyse mécanique (test cisaillement),</a>
+                                </li>
+                                <li id="contenu10"><a href="contenu10.html" title="Essais climatiques">Essais climatiques, environnementaux.</a>
+                                </li>
+                              </ul>
+
+                            </div>
+
+
+                          </div>
+                           <!--<div id="imgd">
+                    
+
+                      <div class="wrap">
+  <div class="cube">
+    <div class="front"><img id="imgd1" src="images/prototype/im3.jpg" alt="Image1"/></div>-->
+    <!--<div class="back"><img id="imgd2" src="images/image2.png" alt="Image2"/></div>
+    <div class="top"><img id="imgd3" src="images/image3.png" alt="Image3"/></div>
+    <div class="bottom">bottom</div>
+    <div class="left">left</div>
+    <div class="right">right</div>-->
+<!--  </div>
+
+</div>
+
+
+                           
+                            
+                          </div>-->
+
+
+
+                          
+                       
+                        <!--fin contenu-->
+                      
+                      <!--pieds de page-->
+                      <footer>
+                        <div id="footer" class="footer text-center">
+                          <div class="container">
+                            <div class="row">
+                              <div class="col-xs-12 col-md-12 col-sm-12 col-lg-12">
+                                <h6 class="section-title center">Partenaires</h6>
+                                <p>
+                                  CQC
+                                </p>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </footer>
+                    </nav>
+                   
+                  </div>
+                </div>
+                <!--google analytics-->
+                <script src="http://www.google-analytics.com/urchin.js" type="text/javascript">
+                  
+                </script>
+                <script type="text/javascript"> _uacct = "UA-738644-5";urchinTracker();
+                </script>
+                <!--fin google analytics--><!-- Incoporez du JavaScript dans la page  -->
+                <script type="text/javascript" src="js/page3.js">
+                  
+                </script>
+                <!--<script>var stream_tab = {
+                  nbSlide : 0,
+                  nbCurrent : 1,
+                  elemCurrent : null,
+                  elem : null,
+                  timer : null,
+                  init : function(elem){
+                    this.nbSlide = elem.find("img").length;
+                    // Initiation du stream_tab
+                    this.elem=elem;
+                    elem.find("img").hide();
+                    elem.find("img:first").show();
+                    this.elemCurrent = elem.find("img:first");
+                    // Timer
+                    this.timer = window.setInterval("stream_tab.next()",2000);},
+                    gotoSlide : function(num){
+                      if(num==this.nbCurrent){return false;}
+                      $('#tab'+(num-1)).fadeOut();
+                      this.elem.find("#tab"+num).fadeIn();
+                      this.nbCurrent = num;
+                      this.elemCurrent = this.elem.find("#pad"+num);},
+                      next : function(){
+                        var num = this.nbCurrent+1;
+                        if(num > this.nbSlide){
+                          num = 1;
+                        }
+                        this.gotoSlide(num); },}
+                        $(function(){
+                        stream_tab.init($("#bg"));});
+                      </script>-->
+             </body>
+      </html>
